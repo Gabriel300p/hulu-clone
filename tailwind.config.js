a15846bbda1c0c26474040a2813dafd80f9ab41c
Gabriel Andrade <gabriel.andrade200p@gmail.com>
2022-01-22 15:24:32 -0300

    1.0.1 - Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,txs}', './components/**/*.{js/ts/jsx/tsx'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

4ac55d73494d11cd34fc489b5d6c098aef432d45
Gabriel Andrade <gabriel.andrade200p@gmail.com>
2022-01-23 18:44:53 -0300

    1.0.3 - Done Header

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,8 +1,8 @@
 function HeaderItem({ Icon, title }) {
   return (
-    <div> 
-      <Icon className="h-8 mb-1" />
-      <p>{title}</p>
+    <div className="group flex flex-col cursor-pointer items-center w-12 sm:w-20 hover:text-white">
+      <Icon className="h-7 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
     </div>
   )
 }
